2dd6d07ddc4c4d096ae56a1fd5b8ba91a8a4ee5b
lucas026-ligther <lucasesluz2@gmail.com>
2025-11-18 21:11:19 -0300

    Delete Main.java

--- Main.java
+++
@@ -1,7 +0,0 @@
-public class Main{
-    public static void main(String[] args){
-Cliente cliente1 = new Cliente("João", "C001","Rua XX, casa YY", "1111-1111", "Joao@email");
-
-Servico servico = new Servico("S001", "Lavagem simples", "Uma lavagem de roupas simples", "25,00", "Em duas horas");
-    }
-}
