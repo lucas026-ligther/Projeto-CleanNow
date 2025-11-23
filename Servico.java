507db5eb1caa6dbf333077620bbbb27c696cb710
lucas026-ligther <lucasesluz2@gmail.com>
2025-11-23 12:15:41 -0300

    Update Servico.java

--- Servico.java
+++ Servico.java
@@ -1,16 +1,38 @@
+package Lavanderia;
+import java.util.Objects;
+
 public class Servico {
     private String id;
     private String nome;
     private String descricao;
-    private double preco;
-    private int prazoEntrega;
+    private double preço;
+    private int prazoEntregaHoras;
     private boolean disponivel;
 
-    public Servico(String id, String nome, String descricao, double preco, int prazoEntrega) {
-        this.id = id;
-        this.nome = nome;
+    public Servico(String id, String nome, String descricao, double preço, int prazoEntregaHoras) {
+        this.id = Objects.requireNonNull(id);
+        this.nome = Objects.requireNonNull(nome);
         this.descricao = descricao;
-        this.preco = preco;
-        this.prazoEntrega = prazoEntrega;
+        this.preço = preço;
+        this.prazoEntregaHoras = prazoEntregaHoras;
+        this.disponivel = true;
+    }
+    public String getId() { return id; }
+    public String getNome() { return nome; }
+    public String getDescricao() { return descricao; }
+    public double getPreco() { return preço; }
+    public int getPrazoEntregaHoras() { return prazoEntregaHoras; }
+    public boolean isDisponivel() { return disponivel; }
+
+    public String getTipo(){return nome;}
+
+    public String toString() {
+        return "Servico{" +
+                "id='" + id + '\'' +
+                ", nome='" + nome + '\'' +
+                ", preco=" + preço +
+                ", prazoEntregaHoras=" + prazoEntregaHoras +
+                '}';
     }
 }
+
