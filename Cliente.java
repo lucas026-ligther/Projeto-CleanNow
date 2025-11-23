0d2be2a1dce8b197958479b733fe56cec278ef9d
lucas026-ligther <lucasesluz2@gmail.com>
2025-11-23 12:14:57 -0300

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -1,20 +1,54 @@
-import java.time.LocalDate;
-import java.util.List;
+import java.util.Objects;
+
 
 public class Cliente {
     private String nome;
     private String id;
-    private String endereco;
+    private String endereço;
     private String telefone;
-    private String email;
-    private List<String> preferências;
-    private LocalDate dataCadastro;
+    private List<String> preferencias;
+    private LocalDateTime dataCadastro;
+    private boolean isVip;
 
-    public Cliente( String NOME, String ID, String ENDERECO, String TELEFONE, String EMAIL) {
-        this.nome = NOME;
-        this.id = ID;
-        this.endereco = ENDERECO;
-        this.telefone = TELEFONE;
-        this.email = EMAIL;
+    public Cliente(String nome,String id,String endereço,String telefone) {
+        this.nome =Objects.requireNonNull(nome,"nome não pode ser nulo");
+        this.id = Objects.requireNonNull(id);
+        this.endereço = endereço;
+        this.telefone = telefone;
+        this.preferencias = new ArrayList<>();
+        this.dataCadastro = LocalDateTime.now();
+        this.isVip = false;
     }
+
+    public String getNome() {return nome;}
+    public void setNome(String nome) {this.nome = nome;}
+
+    public String getId() {return id;}
+
+    public String getEndereço() {return endereço;}
+    public void setEndereço(String endereço) {this.endereço = endereço;}
+
+    public String getTelefone() { return telefone; }
+    public void setTelefone(String telefone) { this.telefone = telefone; }
+
+    public List<String> getPreferencias() { return preferencias; }
+    public void addPreferencia(String p) { preferencias.add(p); }
+
+    public LocalDateTime getDataCadastro() { return dataCadastro; }
+
+    public static boolean isVip() { return isVip; }
+    public void setVip(boolean vip) { this.isVip = vip; }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "nome='" + nome + '\'' +
+                ", id='" + id + '\'' +
+                ", endereco='" + endereço + '\'' +
+                ", telefone='" + telefone + '\'' +
+                ", isVip=" + isVip +
+                ", dataCadastro=" + dataCadastro +
+                '}';
+    }
+
 }
