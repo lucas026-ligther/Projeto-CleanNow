70aecf4b91bba20679e331b6788e61554d549c9f
lucas026-ligther <lucasesluz2@gmail.com>
2025-12-05 21:15:57 -0300

    Update Pacote.java

--- Pacote.java
+++ Pacote.java
@@ -12,8 +12,8 @@ public class Pacote {
     private double preco;
     private boolean isVip;
     private int prioridade;
-
     private LocalDateTime data;
+
     public Pacote(int id, String nome, String descricao) {
         this.id = id;
         this.nome = nome;
@@ -21,62 +21,22 @@ public class Pacote {
         this.servicosInclusos = new ArrayList<>();
         this.isVip = false;
         this.prioridade = 1;
-    }
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
-    public String getNome() {
-        return nome;
-    }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-    public String getDescricao() {
-        return descricao;
-    }
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-    public List<Servico> getServicosInclusos() {
-        return servicosInclusos;
-    }
-    public void setServicosInclusos(List<Servico> servicosInclusos) {
-        this.servicosInclusos = servicosInclusos;
-    }
-    public double getPreco() {
-        return preco;
-    }
-    public void setPreco(double preco) {
-        this.preco = preco;
-    }
-    public boolean isVip() {
-        return isVip;
-    }
-    public void setVip(boolean vip) {
-        isVip = vip;
-    }
-    public int getPrioridade() {
-        return prioridade;
-    }
-    public void setPrioridade(int prioridade) {
-        this.prioridade = prioridade;
+        this.data = LocalDateTime.now();
     }
 
-    public void addServico(Servico servico) {
-        this.servicosInclusos.add(servico);
-    }
-    public void removeServico(Servico servico) {
-        this.servicosInclusos.remove(servico);
+    public double getPreco() { return preco; }
+    public void setPreco(double preco) { this.preco = preco; }
+    public void addServico(Servico servico) { this.servicosInclusos.add(servico); }
+    public void removeServico(Servico servico) { this.servicosInclusos.remove(servico); }
+
+    public double calcularEconomia() {
+        double totalIndividual = 0;
+        for (Servico servico : servicosInclusos) totalIndividual += servico.getPreco();
+        return totalIndividual - preco;
     }
 
-    public double caucularEconomia(){
-double totalIndividual = 0;
-for(Servico servico : servicosInclusos){
-    totalIndividual += servico.getPreco();
-}
-return totalIndividual - preco;
+    @Override
+    public String toString() {
+        return "Pacote{" + nome + ", preco=R$" + preco + "}";
     }
 }
